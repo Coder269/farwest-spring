8d527ed6735d02e220125e7dbf68eaa04a8c61e6
[dervieux.ben@gmail.com](mailto:dervieux.ben@gmail.com) <dervieux.ben@gmail.com>
2023-07-20 15:22:10 +0200

    update project

--- database/wild-code-west.sql
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
--- Active: 1687965389299@@127.0.0.1@3306@wild_code_west
-CREATE DATABASE IF NOT EXISTS `wild_code_west`;
-USE `wild-code-west`;
-=======
--- Active: 1687965389299@@127.0.0.1@3306@wild-code-west
-CREATE DATABASE IF NOT EXISTS `wild_code_west`;
-USE `wild_code_west`;
->>>>>>> dev
